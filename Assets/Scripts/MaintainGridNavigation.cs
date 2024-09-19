4985a7b4753051a788b10f4b42da9c6ee9053b18
Osaze Shears <yoze15@gmail.com>
2024-09-18 20:14:03 -0400

    Updated navigation logic, but need to test it once selectable area switching is implemented

--- Assets/Scripts/MaintainGridNavigation.cs
+++ Assets/Scripts/MaintainGridNavigation.cs
@@ -13,12 +13,15 @@ namespace UserInterface{
 
         [SerializeField] 
 
+        private void Awake() {
+            m_Selectables = new List<Selectable>();
+        }
+
         void Update(){
 
-            if (transform.childCount != previousCount){
+            if (transform.childCount != 0 && transform.childCount != previousCount){
                 GetComponentsInChildren(false,m_Selectables);
                 SetupNavigation();
-
             }
 
             // if ()
@@ -30,6 +33,10 @@ namespace UserInterface{
         /// TODO: setup grid traversal (up, down, left and right)
         /// </summary>
         void SetupNavigation(){
+            if (m_Selectables == null){
+                Debug.LogWarning("Setup navigation was called but no selectables available to configure.");
+                return;
+            }
             for(int i = 0; i < m_Selectables.Count; i++){
                 Navigation nav = new Navigation();
                 nav.selectOnLeft = i > 0 ? m_Selectables[i - 1] : m_Selectables[m_Selectables.Count - 1]; 
