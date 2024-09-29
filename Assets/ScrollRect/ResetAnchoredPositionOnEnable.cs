92d19b8221dde114ca5a67907fbd3309ce89035b
Osaze Shears <yoze15@gmail.com>
2024-09-29 15:04:44 -0400

    Fixed scroll adjusting based on the currently selected selectable

---
+++ Assets/ScrollRect/ResetAnchoredPositionOnEnable.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Extensions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace OSGAmes.UserInterface{
+    public class ResetAnchoredPositionOnEnable : MonoBehaviour
+    {
+
+        void OnEnable(){
+            Vector2 pos = GetComponent<RectTransform>().anchoredPosition;
+            GetComponent<RectTransform>().anchoredPosition = new Vector2(pos.x, 0);
+        }
+
+    }
+
+}
--- Assets/ScrollRect/ScrollRectAdjust.cs
+++ Assets/ScrollRect/ScrollRectAdjust.cs
@@ -4,79 +4,86 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using DG.Tweening;
 
-public class ScrollRectAdjuster : MonoBehaviour
-{
-    [SerializeField] private ScrollRect scrollRect;
-    private RectTransform contentRect;
-    private List<Transform> elements;
-
-    [SerializeField] Transform m_ParentTransform;
-
-
-    void Start()
+namespace OSGAmes.UserInterface{
+    public class ScrollRectAdjuster : MonoBehaviour
     {
-        contentRect = scrollRect.content;
+        [SerializeField] private ScrollRect scrollRect;
+        private RectTransform contentRect;
+        private List<Transform> elements;
+
+        [SerializeField] Transform m_ParentTransform;
+
+        [SerializeField] float m_ContentHeight;
 
 
-        elements = new List<Transform>();
-
-
-        for(int i = 0; i < m_ParentTransform.childCount; i++){
-            elements.Add(m_ParentTransform.GetChild(i));
-        }
+        void Start()
+        {
             
+        } 
 
-
-
-        // for (int k = 0; k < 15; k++)
-        // {
-        //     GameObject go = Instantiate(ElementPrefab, contentRect.transform);
-        //     go.GetComponentInChildren<TextMeshProUGUI>().text = k.ToString();
-        //     elements.Add(go);
-        // }
-        
-        foreach (Transform go in elements)
-        {
-            Debug.Log($"Adding event trigger for select for: {go}");
-            EventTrigger eventTrigger = go.GetComponent<EventTrigger>();
-
-            eventTrigger.AddListener(EventTriggerType.Select, OnMove);
+        void OnEnable(){
+            ConfigureScrollAdjust();
         }
-        scrollRect.verticalNormalizedPosition = 0;
-        // EventSystem.current.SetSelectedGameObject(elements[0]);
-    } 
 
-    private void OnDestroy()
-    {
-        foreach (Transform go in elements)
-        {
-            EventTrigger eventTrigger = go.GetComponentInChildren<EventTrigger>();
-            // eventTrigger.RemoveListener(EventTriggerType.Select, OnSelect);
-            // eventTrigger.RemoveListener(EventTriggerType.UpdateSelected, OnSelect);
-            eventTrigger.RemoveListener(EventTriggerType.Move, OnMove);
+        public void ConfigureScrollAdjust(){
+            contentRect = scrollRect.content;
+
+
+            elements = new List<Transform>();
+
+
+            for(int i = 0; i < m_ParentTransform.childCount; i++){
+                elements.Add(m_ParentTransform.GetChild(i));
+            }
+                
+            foreach (Transform go in elements)
+            {
+                Debug.Log($"Adding event trigger for select for: {go}");
+                EventTrigger eventTrigger = go.GetComponent<EventTrigger>();
+
+                eventTrigger.AddListener(EventTriggerType.Select, OnMove);
+            }
+            scrollRect.verticalNormalizedPosition = 0;
         }
-    }  
 
-    private void OnMove(BaseEventData baseEventData)
-    {
-        // Debug.Log($"new object selected: {baseEventData}");
-        RectTransform selectedRectTransform = baseEventData.selectedObject.GetComponent<RectTransform>();
+        void OnDisable(){
+            ReleaseScrollAdjust();
+        }
 
-        // contentRect.anchoredPosition = new Vector2(contentRect.anchoredPosition.x, -1 * selectedRectTransform.position.y);
-        // contentRect.position = new Vector2(contentRect.anchoredPosition.x, -1 * selectedRectTransform.position.y);
-        contentRect.anchoredPosition = new Vector2(contentRect.anchoredPosition.x, -1 * selectedRectTransform.anchoredPosition.y - 50f);
-        
-        // var height = scrollRect.GetComponent<RectTransform>().rect.height;
-        // var contentHeight = contentRect.rect.height;
-        // var overflow = (contentHeight - height) / 2f; 
-        
-        // var bottomBorder = overflow - selectedRectTransform.offsetMin.y;
-        // var topBorder = -(overflow + (selectedRectTransform.offsetMax.y - contentHeight));
+        public void ReleaseScrollAdjust(){
+            foreach (Transform go in elements)
+            {
+                EventTrigger eventTrigger = go.GetComponentInChildren<EventTrigger>();
+                // eventTrigger.RemoveListener(EventTriggerType.Select, OnSelect);
+                // eventTrigger.RemoveListener(EventTriggerType.UpdateSelected, OnSelect);
+                eventTrigger.RemoveListener(EventTriggerType.Move, OnMove);
+            }
+        }
 
-        // if (bottomBorder > contentRect.anchoredPosition.x)
-        //     contentRect.anchoredPosition = new Vector2(contentRect.anchoredPosition.x, leftBorder);
-        // else if (topBorder < contentRect.anchoredPosition.x)
-        //     contentRect.anchoredPosition = new Vector2(contentRect.anchoredPosition.x, rightBorder);
+        private void OnDestroy()
+        {
+            
+        }  
+
+        private void OnMove(BaseEventData baseEventData)
+        {
+            RectTransform selectedRectTransform = baseEventData.selectedObject.GetComponent<RectTransform>();
+
+
+            // contentRect.anchoredPosition = new Vector2(contentRect.anchoredPosition.x, -1 * selectedRectTransform.anchoredPosition.y - 50f);
+            Vector2 newContentPosition = new Vector2(contentRect.anchoredPosition.x, -1 * selectedRectTransform.anchoredPosition.y);
+            DOTween.To(GetAnchoredPosition,SetAnchoredPosition,newContentPosition,0.25f);
+            // contentRect.anchoredPosition = 
+        }
+
+        Vector2 GetAnchoredPosition(){
+            return contentRect.anchoredPosition;
+        }
+
+        void SetAnchoredPosition(Vector2 newPosition){
+            contentRect.anchoredPosition = newPosition;
+        }
     }
 }
